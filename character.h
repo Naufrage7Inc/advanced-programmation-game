56f38825153fa2312a4cf4f3fa95d4eb85d03f7f
Théo Barragué <naufrage7@gmail.com>
2017-09-25 21:10:50 +0200

    Ajout de fonctions

--- character.h
+++ character.h
@@ -1,6 +1,13 @@
 #ifndef CHARACTER_H_INCLUDED
 #define CHARACTER_H_INCLUDED
 
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
+
 #include <SDL2/SDL.h>
 
 #include "engine.h"
@@ -12,7 +19,12 @@ typedef struct {
     Size size;
     SDL_Surface* surface;
     SDL_Rect srcRect;
+    int direction;
 } Character;
+
+
+/* Permet de créer un personnage */
+Character CharacterCreate(const SDL_Surface* surface, const Coords position, const Size size);
 
 
 #endif // CHARACTER_H_INCLUDED
