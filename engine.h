4c57c19e985ef89431d586bf11937f284fd7b1d1
Théo Barragué <naufrage7@gmail.com>
2017-09-25 20:17:42 +0200

    Ajout du fichier engine.h

---
+++ engine.h
@@ -0,0 +1,19 @@
+#ifndef ENGINE_H_INCLUDED
+#define ENGINE_H_INCLUDED
+
+
+/* Structure pour contenir une coordonée représentée par un point X et un point Y */
+typedef struct {
+    int x;
+    int y;
+} Coords;
+
+
+/* Strucutre pour contenir une taille représentée par sa longueur et sa hauteur */
+typedef struct {
+    int w;
+    int h;
+} Size;
+
+
+#endif // ENGINE_H_INCLUDED
