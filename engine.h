eb4b549746f6c8947d78a23e07a7e26927a0a60e
Théo Barragué <naufrage7@gmail.com>
2017-09-25 21:10:33 +0200

    Ajout de fonctions

--- engine.h
+++ engine.h
@@ -2,6 +2,11 @@
 #define ENGINE_H_INCLUDED
 
 
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+
 /* Structure pour contenir une coordonée représentée par un point X et un point Y */
 typedef struct {
     int x;
@@ -15,5 +20,24 @@ typedef struct {
     int h;
 } Size;
 
+
+/* Permet d'initialiser la SDL */
+bool InitSDL();
+
+
+/* Permet de créer une fenêtre graphique */
+bool CreateWindow(SDL_Window** window, const int width, const int height, const char* title);
+
+
+/* Permet de créer la surface de la fenêtre */
+bool CreateWindowSurface(SDL_Surface** surface, const SDL_Window* window);
+
+
+/* Permet de nettoyer la fenêtre et de décharger la SDL */
+void CleanupSDL(SDL_Window* window);
+
+
+/* Permet de charger une image en mémoire */
+SDL_Surface* LoadBMP(const char* imagePath);
 
 #endif // ENGINE_H_INCLUDED
