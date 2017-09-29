265dc4e75bb17aff1d5c7811f27fb22d4eb13ff1
Théo Barragué <naufrage7@gmail.com>
2017-09-29 12:39:16 +0200

    Utilisation de SurfaceCreate

--- map.c
+++ map.c
@@ -1,24 +1,12 @@
 #include "constants.h"
+#include "engine.h"
 #include "map.h"
 
 
 Map MapCreate( const char* imagePath ) {
     Map map;
 
-    Uint32 rmask, gmask, bmask, amask;
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    rmask = 0xff000000;
-    gmask = 0x00ff0000;
-    bmask = 0x0000ff00;
-    amask = 0x000000ff;
-#else
-    rmask = 0x000000ff;
-    gmask = 0x0000ff00;
-    bmask = 0x00ff0000;
-    amask = 0xff000000;
-#endif
-
-    map.surface = SDL_CreateRGBSurface( 0, WINDOW_WIDTH, WINDOW_HEIGHT, 32, rmask, gmask, bmask, amask );
+    map.surface = SurfaceCreate( WINDOW_WIDTH, WINDOW_HEIGHT );
     map.surfaceTileset = LoadBMP( imagePath );
 
     for ( int y = 0; y < N_BLOCKS_Y; y++ ) {
